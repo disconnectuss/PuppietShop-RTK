afe5af5414c1a71309293fbbd0fc571264e25818
Fatma A <fka201520@gmail.com>
2025-07-28 15:12:30 +0300

    update: config

--- App.jsx
+++ App.jsx
@@ -1,20 +1,12 @@
+import React from 'react';
 import PackageProvider from './src/providers/PackageProvider';
 import StoreProvider from './src/providers/StoreProvider';
 
 const App = () => {
-  const isClient = typeof window !== 'undefined';
-
   return (
-    isClient ? (
-      <StoreProvider>
-        <PackageProvider />
-      </StoreProvider>
-    ) : (
-      // Render the components without providers on the server-side
-      <>
-        {/* Components without providers */}
-      </>
-    )
+    <StoreProvider>
+      <PackageProvider />
+    </StoreProvider>
   );
 };
 
